55822e25e80183f62698d3f48a1773b3770af108
805-16 <805-16@DESKTOP-VDUS7DD>
2024-06-21 11:34:40 +0900

    EX25

---
+++ src/com/javaex/practice/Ex25.java
@@ -0,0 +1,22 @@
+package com.javaex.practice;
+
+public class Ex25 {
+
+	public static void main(String[] args) {
+		/*
+		 * 아래의 출력결과를 예상하여 작성하세요.
+		 * (코드를 작성해서 예상과 맞는지 확인해 보세요.)
+		 *F,T,F,T,T
+		 */
+		int a = 13 ;
+		
+		System.out.println((a>13) && (a>=13) );
+		System.out.println((a>13) || (a>=13));
+		System.out.println((a/3 == 4) && (a%3 == 2));
+		System.out.println((a/3 == 4) || (a%3 == 2));
+		System.out.println((a/3 == 4) && (a%3 == 2) || (a/3 == 4));
+		
+
+	}
+
+}
