9c99771ba27949fac12763c9460e50659bdce1ec
JeffJPortfolio <jeffjeon.official@gmail.com>
2024-06-21 14:36:15 +0900

    Ex 35

---
+++ src/com/javaex/practice/Ex35.java
@@ -0,0 +1,13 @@
+package com.javaex.practice;
+
+public class Ex35 {
+
+	public static void main(String[] args) {
+		// long형 변수를 사용하여 빛이 1년동안 진행하는 거리를 계산하여 출력하세요.
+		
+		long ans = 300000L * 365L * 24L * 60L * 60L;
+		System.out.println("빛이 1년 동안 가는 거리는" + ans + "km 입니다");
+
+	}
+
+}
