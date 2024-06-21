07fd1d5741d52284586946d4a5a7c07f7c849181
805-16 <805-16@DESKTOP-VDUS7DD>
2024-06-21 11:05:59 +0900

    Ex22

---
+++ src/com/javaex/practice/Ex22.java
@@ -0,0 +1,17 @@
+package com.javaex.practice;
+
+public class Ex22 {
+	public static void main(String[] args) {
+		//아래의 출력결과를 예상하여 작성하세요. 
+		//F,T,F,T,F,T,T
+		System.out.println( 17 < 17 );
+		System.out.println( 17 <= 17 );
+		System.out.println( 5 >= 17 );
+		System.out.println( 5 >= 5 );
+		System.out.println( 5 == 17 );
+		System.out.println( 5 != 17 );
+		System.out.println( !(5==17) );
+
+
+	}
+}
